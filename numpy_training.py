532d78410af69661530dc30d3b6d06a436c5cdf0
Morten Bendeke <mbendeke@hotmail.com>
2024-02-21 20:52:06 +0100

    first couple of exercises

---
+++ numpy_training.py
@@ -0,0 +1,36 @@
+#%%
+import math
+import random
+import numpy as np
+#print out cpu time
+import time
+
+
+#%%
+
+# getting a random number
+
+#%%
+rand = random.random()
+start_time = time.time()
+
+# Python list: sin of 100,000,000 random numbers
+pl = [math.sin(random.random()) for i in range(100_000_000)]
+
+end_time = time.time()
+execution_time = end_time - start_time
+
+print("Execution time:", execution_time, "seconds")
+
+#%%
+
+start_time = time.time()
+
+na = np.sin(np.random.rand(100_000_000))
+
+end_time = time.time()
+execution_time = end_time - start_time
+
+print("Execution time:", execution_time, "seconds")
+
+#%%
